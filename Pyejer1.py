cf91da82b677a3cb40abf00722c6c098c9ac50cd
Rdrx11 <rodrox921@gmail.com>
2026-04-13 09:47:35 -0400

    ejemplo_de_variables

--- Pyejer1.py
+++ Pyejer1.py
@@ -1,1 +1,22 @@
-print ("hola Gerges") 
+print ("hola Gerges")
+
+#creando variables
+
+titulo= "clima de hoy" #tipo string
+deiadelmes=13 # int
+mes=4 # int
+
+temperatura=22.3 # float
+
+llueve=False # boolean
+
+print(titulo)
+print("temperatura actual", temperatura, "grados")
+print(deiadelmes, "-", mes)
+
+if llueve:
+    print("tiene que llevar paraguas")
+else:    
+    print("puede llevar polera sin mangas")
+
+
