6aa08d75c6a1fee387a4780cb52e68d4c99e9604
Rdrx11 <rodrox921@gmail.com>
2026-04-20 09:12:40 -0400

    mis preciadas UMAS, OGURI DEJA DE COMER(segundo re)

---
+++ mas ejercios.py
@@ -0,0 +1,12 @@
+trainer="Trainer"
+
+UMAS=6
+
+trainer2="trainer2"
+
+Umas2=6
+
+if UMAS<=6:
+    print("puede participar la carrera, el arima kinen")
+else:
+    print("tiene mas umas de lo necesarios")
