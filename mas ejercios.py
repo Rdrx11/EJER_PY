e3bbd5cd248f69cee6a604b3bcde2fe373445e0b
Rdrx11 <rodrox921@gmail.com>
2026-04-20 11:06:02 -0400

    ejercios con string

--- mas ejercios.py
+++ mas ejercios.py
@@ -1,10 +1,10 @@
-trainer="Trainer"
+# trainer="Trainer"
 
-UMAS=6
+# UMAS=6
 
-trainer2="trainer2"
+# trainer2="trainer2"
 
-Umas2=6
+# Umas2=6
 
 # if UMAS<=6:
 #     print("puede participar la carrera, el arima kinen")
@@ -18,3 +18,46 @@ Umas2=6
 # print("trainer", trainer2, "tiene", Umas2, "UMAMUSUME")
 # print(f"trainer, {trainer} tiene, {UMAS} UMAMUSUME")
 # print(f"trainer, {trainer2} tiene, {Umas2} UMAMUSUME")
+
+# 
+# name="joseph"
+# print(name[-4])
+
+# print(trainer.strip())
+# print(trainer.lower().strip())
+# print(trainer2.upper())
+# print(trainer2.replace("carpenter", " grant"))
+# print(trainer2.find("carpenter"))
+# print(trainer2.split())
+
+
+# pedir la clave al usario y verificar que sea shazam, independientemente de su case
+
+# clave="SHAZAM"
+# code=input("Ingrese la contraseña: ")
+# if clave==code.upper():
+#     print ("bienvenido al sistema")
+# else:
+#     print("error, clave invalida")
+
+
+# crear un nombre de usuario y verificar su largo este entre 4 y 10 caracteres
+
+# nameU=input("cree un nombre de usario: ")
+
+# if len(nameU)>=4 and len(nameU)<=10:
+#     print("usuario creado correctamente")
+# elif len(nameU)>=10:
+#     print("tiene muchas caracteres, use 10 o menos")
+# else:
+#     print("tiene muy pocos caracateres use 4 o mas")
+
+# crear un pin y que este tenga exactamente 4 digitos
+# pin=input("cree su pin: ")
+
+# if len(pin)==4 :
+#     print("pin creado correctamente")
+#     pinn=int(pin)
+# else:
+#     print("no se pase, solo son 4 numeros")
+    
