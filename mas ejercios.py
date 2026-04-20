e88794959eee6a9202e02e300ff56d93739a47ed
Rdrx11 <rodrox921@gmail.com>
2026-04-20 09:40:07 -0400

    concatenaciones

--- mas ejercios.py
+++ mas ejercios.py
@@ -6,7 +6,15 @@ trainer2="trainer2"
 
 Umas2=6
 
-if UMAS<=6:
-    print("puede participar la carrera, el arima kinen")
-else:
-    print("tiene mas umas de lo necesarios")
+# if UMAS<=6:
+#     print("puede participar la carrera, el arima kinen")
+# else:
+#     print("tiene mas umas de lo necesarios")
+ 
+
+# print(len(trainer))
+
+# print("trainer", trainer, "tiene", UMAS, "UMAMUSUME")
+# print("trainer", trainer2, "tiene", Umas2, "UMAMUSUME")
+# print(f"trainer, {trainer} tiene, {UMAS} UMAMUSUME")
+# print(f"trainer, {trainer2} tiene, {Umas2} UMAMUSUME")
