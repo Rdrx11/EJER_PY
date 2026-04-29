0a668f04aa7a66cd430ce977b02559378541778e
Rdrx11 <rodrox921@gmail.com>
2026-04-29 16:59:49 -0400

    waaagh

--- match.py
+++ match.py
@@ -1,6 +1,6 @@
 # ejemplo
-op=0
-total=0
+# op=0
+# total=0
 
 # while op!=4: 
 #     print("1.- radio esterio $70000")
@@ -66,50 +66,101 @@ total=0
 #     print(f"el resultado es: (n1+n2)")
 # op=0
 # while op!<=5:
-name="rod"
-def saludo():
-    print(f"hola {name}")
-def adios():
-    print(f"adios {name}")
+# name="rod"
+# def saludo():
+#     print(f"hola {name}")
+# def adios():
+#     print(f"adios {name}")
 
-def suma():
-    n1=int(input("ingrese un numero: "))
-    n2=int(input("ingrese un numero: "))
-    print(f"el resltado es: {n1+n2}")
-def resta():
-        n1=int(input("ingrese un numero: "))
-        n2=int(input("ingrese un numero: "))
-        print(f"el resltado es: {n1-n2}")
-def multiplicar():
-         n1=int(input("ingrese un numero: "))
-         n2=int(input("ingrese un numero: "))
-         print(f"el resltado es: {n1*n2}")
-def dividir():
-          n1=int(input("ingrese un numero: "))
-          n2=int(input("ingrese un numero: "))
-          print(f"el resltado es: {n1/n2}")
+# def suma():
+#     n1=int(input("ingrese un numero: "))
+#     n2=int(input("ingrese un numero: "))
+#     print(f"el resltado es: {n1+n2}")
+# def resta():
+#         n1=int(input("ingrese un numero: "))
+#         n2=int(input("ingrese un numero: "))
+#         print(f"el resltado es: {n1-n2}")
+# def multiplicar():
+#          n1=int(input("ingrese un numero: "))
+#          n2=int(input("ingrese un numero: "))
+#          print(f"el resltado es: {n1*n2}")
+# def dividir():
+#           n1=int(input("ingrese un numero: "))
+#           n2=int(input("ingrese un numero: "))
+#           print(f"el resltado es: {n1/n2}")
+# op=0
+# while op!=4:
+#   print ("seleccione un porgrama")
+#   print ("1.- suma")
+#   print ("2.- resta")
+#   print ("3.- multiplicar")
+#   print ("4.- dividir")
+#   op=int(input())
+#   match op:
+#         case 1:
+#             saludo()
+#             suma()
+#             adios()
+#         case 2:
+#               saludo()
+#               resta ()
+#               adios ()
+#         case 3:
+#               saludo()
+#               multiplicar()
+#               adios()
+#         case 4:
+#               saludo ()
+#               dividir ()
+#               adios ()
+
+# op=0
+# cantp=0
+# total=0
+# while op!=4:
+#     print("1.- niño $290")
+#     print("2.- adulto $790")
+#     print("3.- adulto mayor $490")
+#     print("4.- salir")
+#     op=int(input("seleccione una opcion: "))
+# match op:
+#     case 1:
+#         print("ha seleccionado niño")
+#         cantn=int(input("cuantos niños van a ingresar: "))
+#         while cantn>1 or cantn<10:
+#             print(" cantidad de niños esta fuera de rango, ingrese una cantidad entre 1 y 10")
+#             cantn=int(input("cuantos niños van a ingresar: "))
+#         cantp+=cantn
+#         total+=290
+#     case 2:
+#         print("ha seleccionado adulto")
+#         canta=int(input("cuantos adultos van a ingresar: "))
+#         while canta>1 or canta<10:
+#             print(" cantidad de adultos esta fuera de rango, ingrese una cantidad entre 1 y 10")
+#             canta=int(input("cuantos adultos van a ingresar: "))
+#         cantp+=canta
+#         total+=790
+#     case 3:
+#         print("ha seleccionado adulto mayor")
+#         cantam=int(input("cuantos adultos mayores van a ingresar: "))
+#         while cantam>1 or cantam<10:
+#             print(" cantidad de adultos mayores esta fuera de rango, ingrese una cantidad entre 1 y 10")
+#             cantam=int(input("cuantos adultos mayores van a ingresar: "))
+#         cantp+=cantam
+#         total+=490
+#     case 4:
+#         print("saliendo...")
+#         print(f"su total a pagar es: ${total}")
+#         print(f"la cantidad de personas es: {cantp}")
+#     case _:
+#         print("opcion invalida")
+
+# preguntar el folio de una entrada a un concierto
+# validar que los folios estén entre 7000 y 21000
+# preguntar si esa en cancha vip, cancha general o tribuna
+# cada entrada vale 40000 pero los impuestos son 
+# vip 1.8, general 1.4 y tribuna 1.2
+# mostrar el valor a pagar al final
+
 op=0
-while op!=4:
-  print ("seleccione un porgrama")
-  print ("1.- suma")
-  print ("2.- resta")
-  print ("3.- multiplicar")
-  print ("4.- dividir")
-  op=int(input())
-  match op:
-        case 1:
-            saludo()
-            suma()
-            adios()
-        case 2:
-              saludo()
-              resta ()
-              adios ()
-        case 3:
-              saludo()
-              multiplicar()
-              adios()
-        case 4:
-              saludo ()
-              dividir ()
-              adios ()
+epreccio=40000
---
+++ repaso_de_for.py
@@ -0,0 +1,8 @@
+# uso repaso for
+# import random
+# num= random.randint(1,10)
+# for i in range (num):
+#     print("hola gente")
+
+# for i in "PRIM":
+#     print(i)
