5036ba04c03c062ffcbd5d4b401965c87cf9dcec
Rdrx11 <rodrox921@gmail.com>
2026-05-06 16:55:41 -0400

    da

---
+++ pool.py
@@ -0,0 +1,9 @@
+
+# while True:
+#     try:
+#         edad=int(input("ingresa tu edad: "))
+#         break
+#     except ValueError as mostrarError:
+#         print("solo debes ingresar numeros enteros")
+#         print(mostrarError)
+   
